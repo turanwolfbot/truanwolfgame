699be48cf1089f6286d50b28771fe7c5dadb0e44
turanwolfbot <turanwolfbot@gmail.com>
2025-12-17 16:36:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+body {
+    background: #000;
+    color: white;
+    text-align: center;
+    font-family: Arial;
+}
+
+.menu {
+    margin-top: 120px;
+}
+
+.btn {
+    display: block;
+    margin: 15px auto;
+    padding: 18px 50px;
+    width: 220px;
+    background: #111;
+    border-radius: 12px;
+    color: white;
+    text-decoration: none;
+    font-size: 22px;
+    border: 2px solid #333;
+    transition: 0.2s;
+}
+
+.btn:hover {
+    background: #222;
+    transform: scale(1.04);
+}
